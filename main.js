0fdb8bcfff4a0a834c2f3148fb4cded12c2b1aa7
Yukixt <107333264+Yukixt@users.noreply.github.com>
2023-04-23 14:24:52 -0300

    Se agrega el Proyecto

---
+++ main.js
@@ -0,0 +1,37 @@
+let listElements = document.querySelectorAll('.list__button--click');
+
+listElements.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+        
+
+    })
+});
+
+let listElementsMañana = document.querySelectorAll('.list__button--click-mañana');
+
+listElementsMañana.forEach(listElement => {
+    listElement.addEventListener('click', ()=>{
+        
+        listElement.classList.toggle('arrow');
+
+        let height = 0;
+        let menu = listElement.nextElementSibling;
+        if(menu.clientHeight == "0"){
+            height=menu.scrollHeight;
+        }
+
+        menu.style.height = `${height}px`;
+        
+
+    })
+});
